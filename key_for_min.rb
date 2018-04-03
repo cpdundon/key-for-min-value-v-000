160f418bb936186a1ea7a917faa847d153f9a7ee
Chris Dundon <chris.dundon@gmail.com>
2018-04-03 15:12:12 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,10 @@
 def key_for_min_value(name_hash)
   first = true
   rtn = nil
-  key_for_min_value.each do |k, v|
+  name_hash.each do |k, v|
     if first
       rtn = k
-    elsif v < key_for_min_value[rtn] 
+    elsif v < name_hash[rtn] 
       rtn = k
     end
   end
